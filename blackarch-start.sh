22ad4a7200852fe01d4ccaaa755fc24066645880
HackTFTP <97189175+HackTFTP@users.noreply.github.com>
2024-02-12 14:49:44 +0000

    Update blackarch-start.sh

--- blackarch-start.sh
+++ blackarch-start.sh
@@ -4,7 +4,7 @@ git clone https://aur.archlinux.org/yay-bin.git
 sudo pacman -R yay-bin  
 useradd onlymake
 sudo chown onlymake yay-bin/
-cd yay-bin
+cd yay-bin/  
 su onlymake
 makepkg -si
 userdel -f onlymake
